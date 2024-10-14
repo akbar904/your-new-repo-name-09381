a09eb595c64563867c69db4cf363319fcec0f5f9
akbar904 <zharfan@walturn.com>
2024-10-14 08:24:40 +0700

    Modify App: your-new-repo-name-09381 - Success (#2)
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files

--- src/lib/features/home/home_viewmodel.dart
+++ src/lib/features/home/home_viewmodel.dart
@@ -1,3 +1,4 @@
+
 import 'package:my_app/app/app.bottomsheets.dart';
 import 'package:my_app/app/app.dialogs.dart';
 import 'package:my_app/app/app.locator.dart';
@@ -5,31 +6,36 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class HomeViewModel extends BaseViewModel {
-  final _dialogService = locator<DialogService>();
-  final _bottomSheetService = locator<BottomSheetService>();
+	final _dialogService = locator<DialogService>();
+	final _bottomSheetService = locator<BottomSheetService>();
 
-  String get counterLabel => 'Counter is: $_counter';
+	List<Map<String, dynamic>> _sp500Metrics = [];
+	
+	List<Map<String, dynamic>> get sp500Metrics => _sp500Metrics;
 
-  int _counter = 0;
+	Future<void> fetchSP500Metrics() async {
+		await Future.delayed(Duration(seconds: 1)); // Simulate network delay
+		_sp500Metrics = [
+			{'symbol': 'AAPL', 'price': 150.00, 'change': 1.5},
+			{'symbol': 'GOOGL', 'price': 2800.00, 'change': 2.0},
+			{'symbol': 'AMZN', 'price': 3400.00, 'change': -1.0},
+		];
+		rebuildUi();
+	}
 
-  void incrementCounter() {
-    _counter++;
-    rebuildUi();
-  }
+	void showDialog() {
+		_dialogService.showCustomDialog(
+			variant: DialogType.infoAlert,
+			title: 'Steve Rocks!',
+			description: 'Check out S&P 500 metrics!',
+		);
+	}
 
-  void showDialog() {
-    _dialogService.showCustomDialog(
-      variant: DialogType.infoAlert,
-      title: 'Steve Rocks!',
-      description: 'Give steve $_counter stars on Github',
-    );
-  }
-
-  void showBottomSheet() {
-    _bottomSheetService.showCustomSheet(
-      variant: BottomSheetType.notice,
-      title: 'title',
-      description: 'desc',
-    );
-  }
+	void showBottomSheet() {
+		_bottomSheetService.showCustomSheet(
+			variant: BottomSheetType.notice,
+			title: 'title',
+			description: 'desc',
+		);
+	}
 }
