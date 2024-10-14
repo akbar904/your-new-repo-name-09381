accc087aa8ab244b270f575c3ff4b57e173e87d0
akbar904 <zharfan@walturn.com>
2024-10-14 08:22:49 +0700

    Update files

--- src/lib/features/home/home_view.dart
+++ src/lib/features/home/home_view.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:stacked/stacked.dart';
@@ -5,79 +6,87 @@ import 'package:stacked/stacked.dart';
 import 'home_viewmodel.dart';
 
 class HomeView extends StackedView<HomeViewModel> {
-  const HomeView({super.key});
+	const HomeView({super.key});
 
-  @override
-  Widget builder(
-    BuildContext context,
-    HomeViewModel viewModel,
-    Widget? child,
-  ) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Gap(50),
-                Column(
-                  children: [
-                    const Text(
-                      'Hello from STEVE x STACKED!',
-                      style: TextStyle(
-                        fontSize: 35,
-                        fontWeight: FontWeight.w900,
-                      ),
-                    ),
-                    const Gap(25),
-                    MaterialButton(
-                      color: Colors.black,
-                      onPressed: viewModel.incrementCounter,
-                      child: Text(
-                        viewModel.counterLabel,
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    MaterialButton(
-                      color: Colors.grey,
-                      onPressed: viewModel.showDialog,
-                      child: const Text(
-                        'Show Dialog',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    MaterialButton(
-                      color: Colors.grey,
-                      onPressed: viewModel.showBottomSheet,
-                      child: const Text(
-                        'Show Bottom Sheet',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+	@override
+	Widget builder(
+		BuildContext context,
+		HomeViewModel viewModel,
+		Widget? child,
+	) {
+		return Scaffold(
+			body: SafeArea(
+				child: Padding(
+					padding: const EdgeInsets.symmetric(horizontal: 25),
+					child: Center(
+						child: Column(
+							mainAxisAlignment: MainAxisAlignment.spaceBetween,
+							children: [
+								const Gap(50),
+								Column(
+									children: [
+										const Text(
+											'S&P 500 Metrics',
+											style: TextStyle(
+												fontSize: 35,
+												fontWeight: FontWeight.w900,
+											),
+										),
+										const Gap(25),
+										FutureBuilder(
+											future: viewModel.fetchSP500Metrics(),
+											builder: (context, snapshot) {
+												if (snapshot.connectionState == ConnectionState.waiting) {
+													return const CircularProgressIndicator();
+												} else if (snapshot.hasError) {
+													return Text('Error: ${snapshot.error}');
+												} else {
+													final metrics = snapshot.data;
+													return Text(
+														'Mock Metrics: $metrics',
+														style: const TextStyle(fontSize: 18),
+													);
+												}
+											},
+										),
+									],
+								),
+								Row(
+									mainAxisAlignment: MainAxisAlignment.spaceBetween,
+									children: [
+										MaterialButton(
+											color: Colors.grey,
+											onPressed: viewModel.showDialog,
+											child: const Text(
+												'Show Dialog',
+												style: TextStyle(
+													color: Colors.white,
+												),
+											),
+										),
+										MaterialButton(
+											color: Colors.grey,
+											onPressed: viewModel.showBottomSheet,
+											child: const Text(
+												'Show Bottom Sheet',
+												style: TextStyle(
+													color: Colors.white,
+												),
+											),
+										),
+									],
+								),
+							],
+						),
+					),
+				),
+			),
+		);
+	}
 
-  @override
-  HomeViewModel viewModelBuilder(
-    BuildContext context,
-  ) =>
-      HomeViewModel();
+	@override
+	HomeViewModel viewModelBuilder(
+		BuildContext context,
+	) =>
+			HomeViewModel();
 }
